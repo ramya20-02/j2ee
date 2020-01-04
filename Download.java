e835bc8d85d01a0d1127d7889d533fe960fb6b97
ramya20-02 <58465849+ramya20-02@users.noreply.github.com>
2020-01-04 09:42:04 +0530

    j2ee hanson

---
+++ Download.java
@@ -0,0 +1,64 @@
+package com.cognizant.servlet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Download
+ */
+@WebServlet("/Download")
+public class Download extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Download() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("Hello World!");
+		System.out.println("Welcome to Programming.");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String filename = "Download.txt";
+		String filepath = "c:\\";
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\" ");
+
+		FileInputStream fileInputStream = new FileInputStream(filepath + filename);
+
+		int i;
+		while ((i = fileInputStream.read()) != -1) {
+			out.write(i);
+		}
+		fileInputStream.close();
+		out.close();
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
